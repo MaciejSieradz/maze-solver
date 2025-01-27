a14105508a1b80d376ee699917172ef672aa944e
Maciej Sieradz <macieks53@gmail.com>
2025-01-27 18:35:48 +0100

    Added cell class

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
-from window import Line, Point, Window
+from window import Cell, Line, Point, Window
 
 def main():
     window = Window(800, 600)
-    window.draw_line(Line(Point(50, 50), Point(80, 80)), "black")
+
+    c = Cell(window)
+    c.has_left_wall = False
+    c.draw(50, 50, 100, 100)
+
+    c = Cell(window)
+    c.has_right_wall = False
+    c.draw(125, 125, 200, 200)
+
+    c = Cell(window)
+    c.has_bottom_wall = False
+    c.draw(225, 225, 250, 250)
+
+    c = Cell(window)
+    c.has_top_wall = False
+    c.draw(300, 300, 500, 500)
+
     window.wait_for_close()
 
 if __name__ == "__main__":
