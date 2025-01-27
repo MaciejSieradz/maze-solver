41a8f7729a9d2221a4913f2ed1dec466c676fc77
Maciej Sieradz <macieks53@gmail.com>
2025-01-27 17:35:48 +0100

    Changing names

--- window.py
+++ window.py
@@ -5,9 +5,9 @@ class Window():
     def __init__(self, width, height) -> None:
         self.__root = Tk()
         self.__root.title("Maze solver")
-        self.canvas = Canvas(self.__root, width=width, height=height)
-        self.canvas.pack()
-        self.running = False
+        self.__canvas = Canvas(self.__root, width=width, height=height)
+        self.__canvas.pack(fill=BOTH, expand=1)
+        self.__running = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
 
     def redraw(self):
@@ -15,9 +15,9 @@ class Window():
         self.__root.update()
 
     def wait_for_close(self):
-        self.running = True
-        while self.running:
+        self.__running = True
+        while self.__running:
             self.redraw()
 
     def close(self):
-        self.running = False
+        self.__running = False
