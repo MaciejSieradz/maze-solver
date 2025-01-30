fe4d5948d69ae7692162ecdd9ba7d14e2bb8acb5
Maciej Sieradz <macieks53@gmail.com>
2025-01-30 13:16:42 +0100

    Added maze class

--- window.py
+++ window.py
@@ -25,37 +25,6 @@ class Window():
     def draw_line(self, line, fill_color = "black"):
         line.draw(self.__canvas, fill_color)
 
-class Cell():
-
-    def __init__(self, win) -> None:
-        self.has_left_wall = True,
-        self.has_right_wall = True,
-        self.has_top_wall = True,
-        self.has_bottom_wall = True
-        self._x1 = None
-        self._x2 = None
-        self._y1 = None
-        self._y2 = None
-        self._win = win
-
-    def draw(self, x1, y1, x2, y2):
-        self._x1 = x1
-        self._x2 = x2
-        self._y1 = y1
-        self._y2 = y2
-        if self.has_left_wall:
-            line = Line(Point(x1, y1), Point(x1, y2))
-            self._win.draw_line(line)
-        if self.has_top_wall:
-            line = Line(Point(x1, y1), Point(x2, y1))
-            self._win.draw_line(line)
-        if self.has_right_wall:
-            line = Line(Point(x2, y1), Point(x2, y2))
-            self._win.draw_line(line)
-        if self.has_bottom_wall:
-            line = Line(Point(x1, y2), Point(x2, y2))
-            self._win.draw_line(line)
-
 class Line():
 
     def __init__(self, p1, p2) -> None:
